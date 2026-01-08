5463bb0a3d003426cd4498586f06ab9c21f65baf
luka.shi <zhaozhaoqueue@gmail.com>
2026-01-09 07:33:53 +0800

    2 more

---
+++ linked_array/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,29 @@
+package linked_array;
+
+
+// core idea: 2 pointers, 2 iterations
+//      each pointer go through both 2 list
+//      if there is an intersection, they will meet
+//      otherwise, 2 pointers will be null at the end
+
+public class IntersectionOfTwoLinkedLists {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // keep the headA and headB
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while(p1 != p2){
+            p1 = p1 == null ? headB : p1.next;
+            p2 = p2 == null ? headA : p2.next;
+        }
+        return p1;
+    }
+}
---
+++ linked_array/SwapNodesInPairs.java
@@ -0,0 +1,33 @@
+package linked_array;
+
+public class SwapNodesInPairs {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        // corner case
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode right = head.next;
+
+        while (right != null){
+            ListNode temp = right.next;
+            head.next = temp;
+            pre.next = right;
+            // move
+            pre = pre.next.next;
+            right = temp.next;
+            head = temp; 
+        }
+        return dummy;
+    }
+}
