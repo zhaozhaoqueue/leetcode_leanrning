8631e3d900c8c9677b6159be67a1b395c1c7605c
Luka Shi <luka.shi@mercaso.com>
2026-01-08 09:01:29 +0800

    optimize

--- array_and_string/MaximumSubarray.java
+++ array_and_string/MaximumSubarray.java
@@ -6,14 +6,25 @@ package array_and_string;
 
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
-        // DP solution
-        int[] dp = new int[nums.length]; //dp[i] means the max sum of sub array ending with i
-        int max = nums[0];
-        dp[0] = nums[0];
-        for (int i = 1; i<nums.length; ++i){
-            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
-            max = Math.max(max, dp[i]);
+        // // DP solution
+        // int[] dp = new int[nums.length]; //dp[i] means the max sum of sub array ending with i
+        // dp[0] = nums[0];
+        // for (int i = 1; i<nums.length; ++i){
+        //     dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+        // }
+        // int max = Integer.MIN_VALUE;
+        // for (int i = 0; i < dp.length; i++){
+        //     max = max > dp[i] ? max : dp[i];
+        // }
+        // return max;
+
+        // Kadane's algorithm: save space
+        int currentMax = nums[0];
+        int globalMax = nums[0];
+        for (int i = 1; i < nums.length; i++){
+            currentMax = (currentMax + nums[i]) > currentMax ? (currentMax + nums[i]) : currentMax;
+            globalMax = currentMax > globalMax ? currentMax : globalMax;
         }
-        return max;
+        return globalMax;
     }
 }
